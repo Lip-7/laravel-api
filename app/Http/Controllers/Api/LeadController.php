da6c8bd6ac24658badd5a4a3b012715280721664
Lip-7 <zhighofficial@gmail.com>
2023-06-19 12:27:50 +0200

    added lead and mail

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Mail\NewContact;
+use App\Models\Lead;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class LeadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            'name' => 'required',
+            'address' => 'required|email',
+            'body' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+        $newLead = new Lead();
+        $newLead->fill($data);
+        $newLead->save();
+
+        Mail::to('hevem93221@bodeem.com')->send(new NewContact($newLead));
+
+
+
+        return response()->json([
+            'success' => true,
+        ]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Lead $lead)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Lead $lead)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Lead $lead)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Lead $lead)
+    {
+        //
+    }
+}
---
+++ resources/views/mail/contact.blade.php
@@ -0,0 +1,7 @@
+<h1>ciao</h1>
+
+<p>
+    Nome: {{$lead->name}}
+    Nome: {{$lead->name}}
+    Nome: {{$lead->name}}
+</p>
